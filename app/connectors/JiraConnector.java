936086d0aefdc69b4bf6bfafa9b9cdd0b7e67e0e
David Rudder <drig@noses.org>
2014-07-08 07:11:14 -0600

    adding reopen support. jira is collecting it, but reporting is not done

--- app/connectors/JiraConnector.java
+++ app/connectors/JiraConnector.java
@@ -32,7 +32,8 @@ public class JiraConnector extends Connector {
 		String jiraURL = ConnectorConfiguration.getValue(scrumMaster, getName(), "jiraURL");
 		String username = ConnectorConfiguration.getValue(scrumMaster, getName(), "username");
 		String password = ConnectorConfiguration.getValue(scrumMaster, getName(), "password");
-		String query= ConnectorConfiguration.getValue(scrumMaster, getName(), "query");
+		String velocityQuery = ConnectorConfiguration.getValue(scrumMaster, getName(), "velocityQuery");
+		String churnQuery = ConnectorConfiguration.getValue(scrumMaster, getName(), "churnQuery");
 		String storyPointsCustomFieldID = ConnectorConfiguration.getValue(scrumMaster, getName(), "storyPointsCustomFieldID");
 		String resolverCustomFieldID = ConnectorConfiguration.getValue(scrumMaster, getName(), "resolverCustomFieldID");
 		
@@ -41,7 +42,7 @@ public class JiraConnector extends Connector {
 		if ((StringUtils.isEmpty(jiraURL)) ||
 				(StringUtils.isEmpty(username)) ||
 				(StringUtils.isEmpty(password)) ||
-				(StringUtils.isEmpty(query))) {
+				(StringUtils.isEmpty(velocityQuery))) {
 			throw new ConnectorException("Jira configuration is not complete");
 		}
 		
@@ -61,68 +62,133 @@ public class JiraConnector extends Connector {
 			JiraClient jiraClient = new JiraClient(jiraURL,creds);
 			
 			for (Sprint sprint: activeSprints) {
-				String mungedQuery = query.replaceAll("%s", "'"+sdf.format(sprint.startDate)+"'");
-				Calendar endDate = Calendar.getInstance();
-				endDate.setTime(sprint.startDate);
-				endDate.add(Calendar.DAY_OF_YEAR, 14);
-				
-				mungedQuery = mungedQuery.replaceAll("%e", "'"+sdf.format(endDate.getTime())+"'");
-				
-				Logger.debug ("Using query "+mungedQuery);
-				
-				SearchResult searchResult = jiraClient.searchIssues(mungedQuery, 1000);
-	
-				List<Issue> issues = searchResult.issues;
-				Logger.debug ("Got "+issues.size()+" issues with query \""+query+"\"");
-				
-				HashMap<String, Float> storyPointsPerResolver = new HashMap<String, Float>();
-				for (Issue issue: issues) {
-					float storyPoints = StringUtils.getFloat(issue.getField("customfield_"+storyPointsCustomFieldID).toString(), (float)0);
-					String resolver = issue.getField("customfield_"+resolverCustomFieldID).toString();
-					Logger.debug ("Story points for issue "+issue.getKey()+"="+storyPoints + " val="+issue.getField("customfield_"+storyPointsCustomFieldID));
-					Logger.debug ("Resolver for issue "+issue.getKey()+"="+resolver);
-					
-					if (storyPoints<=0) {
-						continue;
-					}
-					if (StringUtils.isEmpty(resolver)) {
-						continue;
-					}
-					Float alreadyCountedStoryPoints = storyPointsPerResolver.get(resolver);
-					if (alreadyCountedStoryPoints == null) {
-						alreadyCountedStoryPoints = new Float(0);
-					}
-					alreadyCountedStoryPoints = new Float(alreadyCountedStoryPoints.floatValue()+storyPoints);
-					storyPointsPerResolver.put(resolver, alreadyCountedStoryPoints);
-				}
-			
-				List<EmployeeSprint> employeeSprints = sprint.getEmployeeSprints();
-				Logger.debug (employeeSprints.size()+" employee sprints");
-				for (EmployeeSprint employeeSprint: employeeSprints) {
-					Sprint tmp = employeeSprint.sprint;
-					Logger.debug ("Sprint: "+tmp.name);
-
-					Logger.debug ("Employee: "+employeeSprint.employee.id);
-					Logger.debug ("Employee first name: "+employeeSprint.employee.firstName);
-					Logger.debug ("Employee jira IS: "+employeeSprint.employee.jiraID);
-					Employee employee = employeeSprint.employee;
-					if (StringUtils.isEmpty(employee.jiraID)) {
-						continue;
-					}
-					
-					Float points = storyPointsPerResolver.get(employee.jiraID);
-					Logger.debug ("Assigning "+points+" to "+employee.jiraID);
-					if (points != null) {
-						employeeSprint.storyPointsCompleted = points.floatValue();
-						employeeSprint.save();
-					}
-					
-				}
+				calculateVelocity(velocityQuery, storyPointsCustomFieldID, resolverCustomFieldID, sdf, jiraClient, sprint);
+				calculateChurn(churnQuery, resolverCustomFieldID, sdf, jiraClient, sprint);
 			}
+			
+			
 		} catch (JiraException e) {
 			e.printStackTrace();
 			Logger.warn("Could not access jira at url "+jiraURL, e);
 		}		
+	}
+
+	private void calculateVelocity(String velocityQuery, String storyPointsCustomFieldID, String resolverCustomFieldID, SimpleDateFormat sdf,
+			JiraClient jiraClient, Sprint sprint) throws JiraException {
+		// Calculate velocity
+		String mungedQuery = velocityQuery.replaceAll("%s", "'"+sdf.format(sprint.startDate)+"'");
+		Calendar endDate = Calendar.getInstance();
+		endDate.setTime(sprint.startDate);
+		endDate.add(Calendar.DAY_OF_YEAR, 14);
+		
+		mungedQuery = mungedQuery.replaceAll("%e", "'"+sdf.format(endDate.getTime())+"'");
+		
+		Logger.debug ("Using query "+mungedQuery);
+		
+		SearchResult searchResult = jiraClient.searchIssues(mungedQuery, 1000);
+
+		List<Issue> issues = searchResult.issues;
+		Logger.debug ("Got "+issues.size()+" issues with query \""+velocityQuery+"\"");
+		
+		HashMap<String, Float> storyPointsPerResolver = new HashMap<String, Float>();
+		for (Issue issue: issues) {
+			float storyPoints = StringUtils.getFloat(issue.getField("customfield_"+storyPointsCustomFieldID).toString(), (float)0);
+			String resolver = issue.getField("customfield_"+resolverCustomFieldID).toString();
+			Logger.debug ("Story points for issue "+issue.getKey()+"="+storyPoints + " val="+issue.getField("customfield_"+storyPointsCustomFieldID));
+			Logger.debug ("Resolver for issue "+issue.getKey()+"="+resolver);
+			
+			if (storyPoints<=0) {
+				continue;
+			}
+			if (StringUtils.isEmpty(resolver)) {
+				continue;
+			}
+			Float alreadyCountedStoryPoints = storyPointsPerResolver.get(resolver);
+			if (alreadyCountedStoryPoints == null) {
+				alreadyCountedStoryPoints = new Float(0);
+			}
+			alreadyCountedStoryPoints = new Float(alreadyCountedStoryPoints.floatValue()+storyPoints);
+			storyPointsPerResolver.put(resolver, alreadyCountedStoryPoints);
+		}
+
+		List<EmployeeSprint> employeeSprints = sprint.getEmployeeSprints();
+		Logger.debug (employeeSprints.size()+" employee sprints");
+		for (EmployeeSprint employeeSprint: employeeSprints) {
+			Sprint tmp = employeeSprint.sprint;
+			Logger.debug ("Sprint: "+tmp.name);
+
+			Logger.debug ("Employee: "+employeeSprint.employee.id);
+			Logger.debug ("Employee first name: "+employeeSprint.employee.firstName);
+			Logger.debug ("Employee jira IS: "+employeeSprint.employee.jiraID);
+			Employee employee = employeeSprint.employee;
+			if (StringUtils.isEmpty(employee.jiraID)) {
+				continue;
+			}
+			
+			Float points = storyPointsPerResolver.get(employee.jiraID);
+			Logger.debug ("Assigning "+points+" to "+employee.jiraID);
+			if (points != null) {
+				employeeSprint.storyPointsCompleted = points.floatValue();
+				employeeSprint.save();
+			}
+			
+		}
+	}
+	
+	private void calculateChurn(String churnQuery, String resolverCustomFieldID, SimpleDateFormat sdf, JiraClient jiraClient, Sprint sprint) throws JiraException {
+		// Calculate velocity
+		String mungedQuery = churnQuery.replaceAll("%s", "'"+sdf.format(sprint.startDate)+"'");
+		Calendar endDate = Calendar.getInstance();
+		endDate.setTime(sprint.startDate);
+		endDate.add(Calendar.DAY_OF_YEAR, 14);
+		
+		mungedQuery = mungedQuery.replaceAll("%e", "'"+sdf.format(endDate.getTime())+"'");
+		
+		Logger.debug ("Using query "+mungedQuery);
+		
+		SearchResult searchResult = jiraClient.searchIssues(mungedQuery, 1000);
+
+		List<Issue> issues = searchResult.issues;
+		Logger.debug ("Got "+issues.size()+" issues with query \""+churnQuery+"\"");
+		
+		HashMap<String, Integer> reopensPerResolver = new HashMap<String, Integer>();
+		for (Issue issue: issues) {
+			String resolver = issue.getField("customfield_"+resolverCustomFieldID).toString();
+			Logger.debug ("Resolver for issue "+issue.getKey()+"="+resolver);
+			if (StringUtils.isEmpty(resolver)) {
+				continue;
+			}
+
+			Integer countedReopens = reopensPerResolver.get(resolver);
+			if (countedReopens == null) {
+				countedReopens = new Integer(0);
+			}
+			countedReopens = new Integer(countedReopens.intValue()+1);
+			reopensPerResolver.put(resolver, countedReopens);
+		}
+
+		List<EmployeeSprint> employeeSprints = sprint.getEmployeeSprints();
+		Logger.debug (employeeSprints.size()+" employee sprints");
+		for (EmployeeSprint employeeSprint: employeeSprints) {
+			Sprint tmp = employeeSprint.sprint;
+			Logger.debug ("Sprint: "+tmp.name);
+
+			Logger.debug ("Employee: "+employeeSprint.employee.id);
+			Logger.debug ("Employee first name: "+employeeSprint.employee.firstName);
+			Logger.debug ("Employee jira IS: "+employeeSprint.employee.jiraID);
+			Employee employee = employeeSprint.employee;
+			if (StringUtils.isEmpty(employee.jiraID)) {
+				continue;
+			}
+			
+			Integer reopens = reopensPerResolver.get(employee.jiraID);
+			Logger.debug ("Assigning "+reopens+" to "+employee.jiraID);
+			if (reopens != null) {
+				employeeSprint.numReopens = reopens.intValue();
+				employeeSprint.save();
+			}
+			
+		}
 	}
 	
 	public List<String> getParameterNames() {
@@ -130,7 +196,8 @@ public class JiraConnector extends Connector {
 		paramNames.add("jiraURL");
 		paramNames.add("username");
 		paramNames.add("password");
-		paramNames.add("query");
+		paramNames.add("velocityQuery");
+		paramNames.add("churnQuery");
 		paramNames.add("storyPointsCustomFieldID");
 		paramNames.add("resolverCustomFieldID");
 		return paramNames;
